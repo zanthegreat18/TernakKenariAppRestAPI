ef7b146f7818e32d3ccf2f8b3a669d8c0f0d2f19
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-06-06 21:58:37 +0700

    class admin profile

--- kanariapp/lib/data/model/response/buyer/admin_profile_response_model.dart
+++ kanariapp/lib/data/model/response/buyer/admin_profile_response_model.dart
@@ -27,3 +27,27 @@ class AdminProfileResponseModel {
         "data": data?.toMap(),
     };
 }
+
+class AdminProfile {
+    final int? id;
+    final String? name;
+
+    AdminProfile({
+        this.id,
+        this.name,
+    });
+
+    factory AdminProfile.fromJson(String str) => AdminProfile.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory AdminProfile.fromMap(Map<String, dynamic> json) => AdminProfile(
+        id: json["id"],
+        name: json["name"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+    };
+}
