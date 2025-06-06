37a715851c1a4c14bbd4c49c19a4e7df185bc670
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-06-06 22:01:00 +0700

    class user

--- kanariapp/lib/data/model/response/auth_response_model.dart
+++ kanariapp/lib/data/model/response/auth_response_model.dart
@@ -27,3 +27,39 @@ class AuthResponseModel {
         "data": user?.toMap(),
     };
 }
+
+class User {
+    final int? id;
+    final String? name;
+    final String? email;
+    final String? role;
+    final String? token;
+
+    User({
+        this.id,
+        this.name,
+        this.email,
+        this.role,
+        this.token,
+    });
+
+    factory User.fromJson(String str) => User.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        role: json["role"],
+        token: json["token"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "role": role,
+        "token": token,
+    };
+}
